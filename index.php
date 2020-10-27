efb261af02513ca7283a6f4277f91d74bda18d48
Dawid <49307038+DawidRokita@users.noreply.github.com>
2020-10-27 14:49:15 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -7,23 +7,49 @@
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
-    <?php
-        $servername="127.0.0.1";
-        $username="Dawid";
-        $password="Dawid";
-        $dbname="bibliotreka";
+    <div class="grid">
+        <div class="item1">
+            <h1>DAWID ROKITA</h1>
+        </div>
+        <div class="item2">
+            <ul>
+                <li><a href="">LINK1</a></li>
+                <li><a href="">LINK2</a></li>
+            </ul>
+        </div>
+        <div class="item3">
+            <form action="insert.php" method="POST">
+                <ul>
+                    <li>name:<input type="text" name="name"></li>
+                    <li>tytyl:<input type="text" name="tytul"></li>
+                    <li><input type="submit" value="INSERT"></li>
+                </ul>
+            </form>
+        </div>
+        <div class="item4">
+        <!-- <?php
+            echo("START");
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
+            $servername="127.0.0.1";
+            $username="Dawid";
+            $password="Dawid";
+            $dbname="bibliotreka";
 
-        $result = $conn->query();
+            $conn = new mysqli($servername, $username, $password, $dbname);
 
-        echo("<table>");
-        while($wiersz = $result->fetch_assoc()){
-            echo("tr");
-            echo("<td>".$wiersz['']."</td>");
-            echo("/tr");
-        }
-        echo("</table>");
-    ?>
+            $result = $conn->query();
+            
+            echo("<table>");
+            while($wiersz = $result->fetch_assoc()){
+                echo("tr");
+                echo("<td>".$wiersz['']."</td>");
+                echo("/tr");
+            }
+            echo("</table>");
+        ?> -->
+        </div>
+    </div>
+
+    
 </body>
 </html>
