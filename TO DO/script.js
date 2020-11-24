c508599e925ff5f6765d8816b27d19c505c52eaf
Dawid <49307038+DawidRokita@users.noreply.github.com>
2020-11-24 14:44:47 +0100

    Update script.js

--- TO DO/script.js
+++ TO DO/script.js
@@ -4,7 +4,7 @@ const lista = document.querySelector(".lista");
 
 
 dodaj.addEventListener('click', function(){
-    lista.innerHTML =  lista.innerHTML + "<li>" + tekst.value + "<a class='del'> X</a>" + "</li>";
+    lista.innerHTML =  lista.innerHTML + "<li>" + tekst.value + "<a class='del'> X</a></li>";
     tekst.value = "";
 })
 
