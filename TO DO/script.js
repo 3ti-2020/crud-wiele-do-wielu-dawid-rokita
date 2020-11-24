b6959f8380a13b663920d0f36e8d26237be370aa
Dawid <49307038+DawidRokita@users.noreply.github.com>
2020-11-24 14:44:03 +0100

    Add files via upload

--- TO DO/script.js
+++ TO DO/script.js
@@ -4,6 +4,7 @@ const lista = document.querySelector(".lista");
 
 
 dodaj.addEventListener('click', function(){
-    lista.innerHTML =  lista.innerHTML + "<ul><li>" + tekst.value + "</li></ul>";
+    lista.innerHTML =  lista.innerHTML + "<li>" + tekst.value + "<a class='del'> X</a>" + "</li>";
+    tekst.value = "";
 })
 
