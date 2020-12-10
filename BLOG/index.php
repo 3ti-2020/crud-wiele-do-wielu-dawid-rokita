4c1c33e387cde706e7a83679f132f2e62210c41a
Dawid <49307038+DawidRokita@users.noreply.github.com>
2020-12-10 13:14:11 +0100

    Add files via upload

--- BLOG/index.php
+++ BLOG/index.php
@@ -15,19 +15,26 @@
 
         $conn = new mysqli($servername, $username, $password, $dbname);
     
-        $result = $conn->query("SELECT * FROM `posty_tagi`, posty, tagi WHERE posty_tagi.id_posty = posty.id AND posty_tagi.id_tagi = tagi.id");
-        echo("<table>");
-            echo("<tr class='head'>
-                <td>tytul</td>
-                <td>tekst</td>
-                <td>tag</td>
-            </tr>");
-            while($wiersz = $result->fetch_assoc()){
-                echo("<tr class='son'>");
-                echo("<td>".$wiersz['tytul']."</td><td>".$wiersz['tekst']."</td><td>".$wiersz['nazwa']."</td>");
-                echo("</tr>");
-            }
-            echo("</table>");
+        $result = $conn->query("SELECT tytul, tekst, nazwa FROM `posty_tagi`, posty, tagi WHERE posty_tagi.id_posty = posty.id AND posty_tagi.id_tagi = tagi.id");
+
     ?>
+
+    <div class="container">
+
+    <?php
+            while($wiersz = $result->fetch_assoc()){
+                echo("<div class='header'>");
+                    echo("<h1>".$wiersz['tytul']."</h1>");
+                    echo("<h3>".$wiersz['nazwa']."</h3>");
+                echo("</div>");
+                echo("<div class='main'>");
+                    echo("<p>".$wiersz['tekst']."</p>");
+                echo("</div>");
+                // echo("<tr class='son'>");
+                // echo("<td>".$wiersz['tytul']."</td><td>".$wiersz['tekst']."</td><td>".$wiersz['nazwa']."</td>");
+                // echo("</tr>");
+            }
+    ?>
+      </div>
 </body>
 </html>
