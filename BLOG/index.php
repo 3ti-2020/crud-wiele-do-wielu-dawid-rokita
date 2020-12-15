01522094a4c575669fc3506831c6cad2059e850f
Dawid <49307038+DawidRokita@users.noreply.github.com>
2020-12-15 15:22:13 +0100

    Add files via upload

--- BLOG/index.php
+++ BLOG/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
@@ -25,23 +26,29 @@
 
 <div class="container">
     <div class="a">
+    <div class="menu">
+        <a href="../index.php" class="btn 3">home <i class="fas fa-home"></i></a>
+        <a href="https://github.com/3ti-2020/crud-wiele-do-wielu-dawid-rokita"  class="btn" class="btn 1">Github <i class="fab fa-github"></i></a>
+    </div>
+    <div class="tytul">
         <h1>BLOG</h1>
+    </div>
     </div>
     <div class="b">
         <?php
-                while($wiersz = $result->fetch_assoc()){
-                    echo("<div class='header'>");
-                        echo("<h1>".$wiersz['tytul']."</h1>");
-                        $posty = $wiersz["id"];
-                        $result2 = $conn->query("SELECT nazwa FROM `posty_tagi`, posty, tagi WHERE posty_tagi.id_posty = posty.id AND posty_tagi.id_tagi = tagi.id AND posty_tagi.id_posty = $posty");
-                    while($wiersz2 = $result2->fetch_assoc()){
-                        echo("<tr><b><a href='?akcja=".$wiersz2['nazwa']."'>".$wiersz2['nazwa']."</a></b></tr>, ");
-                    } 
-                    echo("</div>");
-                    echo("<div class='main'>");
-                        echo("<p>".$wiersz['tekst']."</p>");
-                    echo("</div>");
-                }
+            while($wiersz = $result->fetch_assoc()){
+                echo("<div class='header'>");
+                    echo("<h1>".$wiersz['tytul']."</h1>");
+                    $posty = $wiersz["id"];
+                    $result2 = $conn->query("SELECT nazwa FROM `posty_tagi`, posty, tagi WHERE posty_tagi.id_posty = posty.id AND posty_tagi.id_tagi = tagi.id AND posty_tagi.id_posty = $posty");
+                while($wiersz2 = $result2->fetch_assoc()){
+                    echo("<tr><b><a href='?akcja=".$wiersz2['nazwa']."'>".$wiersz2['nazwa']."</a></b></tr>, ");
+                } 
+                echo("</div>");
+                echo("<div class='main'>");
+                    echo("<p>".$wiersz['tekst']."</p>");
+                echo("</div>");
+            }
         ?>
     </div>
 </div>
