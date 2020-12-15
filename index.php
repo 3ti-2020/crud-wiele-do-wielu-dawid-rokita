fafe4228681ca06d0f3247c0818dad2eb5466c11
Dawid <49307038+DawidRokita@users.noreply.github.com>
2020-12-15 14:58:37 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -21,13 +21,13 @@
         <div class="item1">
             <div class="menu">
                 <button class="fas fa-bars leftmenu"></button>
-                <a href="./egzamin/index.html" class="btn 5">egzamin <i class="fas fa-graduation-cap"></i></a>
-                <a href="https://github.com/3ti-2020/crud-wiele-do-wielu-dawid-rokita"  class="btn" class="btn 1">github <i class="fab fa-github"></i></a>
-                <a href="#" class="robocze btn 2">tabele <i class="fas fa-table"></i></a>
+                <a href="./egzamin/index.html" class="btn 5">Egzamin <i class="fas fa-graduation-cap"></i></a>
+                <a href="https://github.com/3ti-2020/crud-wiele-do-wielu-dawid-rokita"  class="btn" class="btn 1">Github <i class="fab fa-github"></i></a>
+                <a href="#" class="robocze btn 2">Tabele <i class="fas fa-table"></i></a>
                 <a href="./TO DO/index.html" class="btn 3">ToDo <i class="fas fa-list-ul"></i></a>
-                <a href="./karta/karta.html" class="btn 4">karta <i class="fas fa-address-card"></i></a>
-                <a href="./login/login2.php" class="btn 5">wypożycz <i class="fas fa-book"></i></a>
-                <a href="./BLOG/index.php" class="btn 6">BLOG <i class="fas fa-book"></i></a>
+                <a href="./karta/karta.html" class="btn 4">Karta <i class="fas fa-address-card"></i></a>
+                <a href="./login/login2.php" class="btn 5">Wypożycz <i class="fas fa-book"></i></a>
+                <a href="./BLOG/index.php" class="btn 6">Blog <i class="fab fa-blogger"></i></a>
             </div>
             <div class="tytul">
                 <h1>DAWID ROKITA GR.2</h1>
